10de48a38e63f7e89905945df902a719e87e4c0e
zaba7 <tomizaba99@gmail.com>
2023-03-30 01:08:31 -0300

    primer subida

---
+++ Api_actualizada/Controllers/BCRAController.cs
@@ -0,0 +1,20 @@
+﻿using Api_actualizada.Modelo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_actualizada.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BCRAController : ControllerBase
+    {
+        [HttpGet("{input}")]
+        public IActionResult GetString(string input)
+        {
+            ResultadoBCRA resultado = new ResultadoBCRA();
+            resultado.Fecha = "27/03/2023";
+            resultado.Informacion = input.ToUpper();
+            return Ok(resultado);
+        }
+    }
+}
---
+++ Api_actualizada/Modelo/ResultadoBCRA.cs
@@ -0,0 +1,8 @@
+﻿namespace Api_actualizada.Modelo
+{
+    public class ResultadoBCRA
+    {
+        public string Fecha { get; set; }
+        public string Informacion { get; set; }
+    }
+}
